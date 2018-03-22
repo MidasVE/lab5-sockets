bf05a6bfcf6113d78fee75e2cae871d8d881653a
MidasVE <melkvat@gmail.com>
2018-03-22 14:59:32 +0100

    Added click eventlistener

---
+++ public/javascripts/app.js
@@ -0,0 +1,8 @@
+document.querySelector('.answer__link--1').addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log('1 clicked')
+})
+document.querySelector('.answer__link--2').addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log('2 clicked')
+})
