ad553756f2075955ecfff3276d4b2b8f143663e5
MidasVE <midas.van.espen@outlook.com>
2018-03-31 17:06:01 +0200

    Added primus writer to p1 and added button event listener to createPoll

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -6,6 +6,12 @@ var primus = Primus.connect(url, {
         , retries: 10
     }
 });
+
+document.querySelector('.buttonCreatePoll').addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log('test')
+})
+
 var i = 0;
 var j = 0;
 primus.on('data', function (data) {
@@ -16,17 +22,25 @@ primus.on('data', function (data) {
         j++;
     }
     var p1 = document.querySelector('.p--1');
-    p1.innerHTML = i/(i+j)*100 + '%';
+    var percentage1 = i / (i + j) * 100 + '%';
+    primus.write({p: percentage1})
 
     var p2 = document.querySelector('.p--2')
-    p2.innerHTML = j/(i+j)*100 + '%';
-})
+    p2.innerHTML = j / (i + j) * 100 + '%';
 
-document.querySelector('.answer__link--1').addEventListener('click', function (e) {
-    e.preventDefault();
-    primus.write({action: 'click1'})
+    if (data.p) {
+        console.log(data.p);
+    }
 })
-document.querySelector('.answer__link--2').addEventListener('click', function (e) {
+var answerLink1 = document.querySelector('.answer__link--1');
+if (answerLink1) {
+    answerLink1.addEventListener('click', function (e) {
+        e.preventDefault();
+        primus.write({action: 'click1'})
+    })
+}
+var answerLink2 = document.querySelector('.answer__link--2');
+answerLink2.addEventListener('click', function (e) {
     e.preventDefault();
     primus.write({action: 'click2'})
 })
